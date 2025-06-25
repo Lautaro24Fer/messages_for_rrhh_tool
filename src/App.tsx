9dd41fbfc18b763c8214446b82f453c808144eed
lautaro <lautaroleonelfernandez3@gmail.com>
2025-06-25 15:17:02 -0300

    Arreglo de forms y agregacion de campos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 // src/App.jsx
 import FormMessage from './components/Form'
+import "./App.css"
 
 export default function App() {
   return (
-    <>
-    <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
-    <div className="absolute left-1/2 transform -translate-x-1/2 h-[max] w-max flex flex-col items-center mt-[10rem]">
-      <h1 className="text-2xl font-bold text-center text-3xl">Generador de Mensajes Laborales</h1>
+    <main className="absolute left-1/2 transform -translate-x-1/2 w-screen md:w-[80%] h-[max] flex flex-col items-center">
+      <h1 className="text-2xl font-bold text-center text-3xl">Searching job tool👨‍💼</h1>
       <FormMessage />
-    </div>
-    </>
+    </main>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,6 +8,9 @@ export default function formMessage() {
   const { register, handleSubmit } = useForm<FormData>({
   defaultValues: {
     name: '',
+    age: '',
+    yearsExp: '',
+    degree: '',
     position: '',
     company: '',
     stack: '',
@@ -21,11 +24,16 @@ export default function formMessage() {
   interface FormData {
     type: 'presentation' | 'thanks' | 'first_contact';
     name: string;
+    age: string;
+    yearsExp: string;
+    degree: string;
     position: string;
     company: string;
     stack: string;
     recruiter: string;
     phone: string;
+    email: string;
+    portfolio: string;
     action: 'download' | 'whatsapp';
   }
 
@@ -37,25 +45,39 @@ export default function formMessage() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-[20rem]">
-      <select {...register('type')} className="select">
-        <option value="presentation">Carta Presentación</option>
-        <option value="thanks">Agradecimiento</option>
-        <option value="first_contact">Primer Contacto</option>
-      </select>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full px-4 lg:w-max">
+      <div className="grid md:grid-cols-2 gap-4">
+        <div className="flex flex-col gap-4">
+        
+          <input placeholder="Fullname" {...register('name')} className="input input-neutral" />
+          <input type='number' placeholder="Age" {...register('age')} className="input input-neutral" />
+          <input placeholder="Position" {...register('position')} className="input input-neutral" />
+          <input placeholder="Company" {...register('company')} className="input input-neutral" />
+          <input placeholder="Stack (Separated by comma (,))" {...register('stack')} className="input input-neutral" />
+          <input placeholder="Recruiter" {...register('recruiter')} className="input input-neutral" />
+          <input placeholder="Phone" {...register('phone')} className="input input-neutral" />
 
-      <input placeholder="Name" {...register('name')} className="input input-neutral" />
-      <input placeholder="Position" {...register('position')} className="input input-neutral" />
-      <input placeholder="Company" {...register('company')} className="input input-neutral" />
-      <input placeholder="Stack (Separated by comma (,))" {...register('stack')} className="input input-neutral" />
-      <input placeholder="Recruiter" {...register('recruiter')} className="input input-neutral" />
-      <input placeholder="Phone" {...register('phone')} className="input input-neutral" />
-
-      <select {...register('action')} className="select">
-        <option value="download">Descargar .txt</option>
-        <option value="whatsapp">Enviar por WhatsApp</option>
-      </select>
-
+        </div>
+        <div className="flex flex-col gap-4">
+          <input type='number' placeholder="Years of experience" {...register('yearsExp')} className="input input-neutral" />
+          <input placeholder="Degree" {...register('degree')} className="input input-neutral" />
+          <input type='email' placeholder="Email" {...register('email')} className="input input-neutral" />
+          <input placeholder="Porfolio url" {...register('portfolio')} className="input input-neutral" />
+        </div>
+        <select {...register('type')} className="select">
+            <option value="presentation">Carta Presentación</option>
+            <option value="thanks">Agradecimiento</option>
+            <option value="first_contact">Primer Contacto</option>
+          </select>
+          <select {...register('action')} className="select">
+            <option value="download">Descargar .txt</option>
+            <option value="whatsapp">Enviar por WhatsApp</option>
+          </select>
+      </div>
+      <div className="col">
+          <p className="text-xs text-gray-500">* Los campos son opcionales, pero si no se completan, el mensaje no se generará correctamente.</p>
+          <p className="text-xs text-gray-500">* El número de teléfono debe incluir el código de país (ejemplo: +34 para España).</p>
+        </div>
       <button type="submit" className="bg-blue-500 h-[3rem] cursor-pointer text-white p-2 rounded">Generate</button>
     </form>
   );
